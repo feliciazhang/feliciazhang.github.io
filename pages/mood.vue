cd5e433977004daee07e740d14a17b56e25c0f94
Felicia Zhang <zhang.fel@husky.neu.edu>
2019-12-11 22:15:29 -0500

    multiple mood

--- pages/mood.vue
+++ pages/mood.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="mood">
-    <img class="main-board" src="~assets/mood/2019-11.png" />
+    <ul class="mood-list">
+      <li
+        v-for="item in moodboards"
+        :key="item.title"
+        class="mood-list-item"
+      >
+        <span @click="selectedMood = item">{{ item.title }}</span>
+      </li>
+    </ul>
+
+    <img class="main-board" :src="require('../assets/mood/' + selectedMood.file)" />
   </div>
 </template>
 
@@ -8,6 +18,27 @@
   .mood {
     margin-top: 54px;
     display: flex;
+    overflow: hidden;
+  }
+
+  .mood-list {
+    height: calc(100vh - 54px);
+    overflow-y: auto;
+    width: 250px;
+    list-style-type: none;
+    padding: 16px 24px;
+    background-color: var(--main-navy);
+    color: var(--main-neutral);
+    margin: 0;
+  }
+
+  .mood-list-item {
+    margin: 4px 0;
+    cursor: pointer;
+  }
+
+  .mood-list-item:hover {
+    text-decoration: underline;
   }
 
   .main-board {
@@ -15,3 +46,16 @@
     margin: auto;
   }
 </style>
+
+<script>
+import moodData from '~/assets/mood.json'
+
+export default {
+  data: () => (
+    {
+      moodboards: moodData,
+      selectedMood: moodData[0]
+    }
+  )
+}
+</script>
